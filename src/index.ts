3f10d20242126e928c13cefd7ec3f65abc2f06c4
kodurin <nagarjuna.6062@gmail.com>
2022-07-06 18:51:32 +0530

    commit5 -full payload

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,25 @@ const inputName = getInput("name");
 
 welcome(inputName, repoUrl(context));
 
+console.log('--- Action Info ---')
+console.log('eventName:', context.eventName)
+console.log('sha:', context.sha)
+console.log('ref:', context.ref)
+console.log('workflow:', context.workflow)
+console.log('action:', context.action)
+console.log('actor:', context.actor)
+console.log('job:', context.job)
+console.log('runNumber:', context.runNumber)
+console.log('runId:', context.runId)
+console.log('apiUrl:', context.apiUrl)
+console.log('serverUrl:', context.serverUrl)
+console.log('graphqlUrl:', context.graphqlUrl)
+console.log('-------------------')
+
+console.log('--- Payload ---')
+console.log(JSON.stringify(context.payload, undefined, 2))
+console.log('---------------')
+
 function welcome(name:string, repoUrl: string) {
     console.log(`'Hello ${name} You are running github actions in ${repoUrl}'`);
 }
